584b13d53e24d8696fceb59c36374c2a99a16ed4
Chris Marken <11192801+cmarken@users.noreply.github.com>
2024-01-06 15:21:54 +0100

    Finishing the game, it plays over 5 rounds
    The game can end a draw.

--- js/script.js
+++ js/script.js
@@ -36,18 +36,11 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return 'Computer wins!';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'Player wins';
+        return 'Player wins!';
     } else {
         return 'Something went wrong. I don\'t know who won!?';
     }
 }
-
-/*
-const playerSelection = "scissors";
-const computerSelection = getComputerSelection();
-console.log('Player: ' + playerSelection + ' | Computer: ' + computerSelection + '\n');
-console.log(playRound(playerSelection, computerSelection));
-*/
 
 // Used to contain playRound. Should make us play 5 games in a row. Keep score of
 // winner.
@@ -55,13 +48,53 @@ console.log(playRound(playerSelection, computerSelection));
 // Display the winner after all games.
 function game() {
     // Set up array to keep track of winner
+    let winner = [];
 
     // Loop until we have played 5 games
+    for (let i = 1; i <= 5; i++) {
         // Ask player for the selection and store it in playerSelection
+        const playerSelection = prompt('What is your slection?');
+        
         // get the computerSelection and store it
-        // Display the winner of the round
+        const computerSelection = getComputerSelection();
 
-    // Count the number of player wins and print it
-    // Count the number of computer wins and print it
+        // evaluate who won and store it in winner array
+        winner[i] = playRound(playerSelection, computerSelection);
+
+        // Display the winner of the round
+        console.log(winner[i]); 
+    }
+
+    // Count number of wins for player and computer
+    const playerWins = winner.filter(w => w === 'Player wins!').length;
+    const computerWins = winner.filter(w => w === 'Computer wins!').length;
+    const draws = winner.filter(w => w === 'It is a draw!').length;
+
+
+    // Count the number of player wins and computer wins and draws and print it
+    console.log('Game Over!\nPlayer Wins: ' + playerWins + 
+        '\nComputer Wins: ' + computerWins +
+        '\nDraws: ' + draws);
+
+    // Find out who the total winner is
+    let totalWinner;
+    if (playerWins > computerWins) {
+        totalWinner = 'Player';  
+    } else if (playerWins < computerWins) {
+        totalWinner = 'Computer';
+    } else {
+        totalWinner = 'Noone';
+    }
+
     // Display the total winner
+    console.log('\nTotal winner is: ' + totalWinner);
 }
+
+game();
+
+/* For testing playRound
+const playerSelection = "scissors";
+const computerSelection = getComputerSelection();
+console.log('Player: ' + playerSelection + ' | Computer: ' + computerSelection + '\n');
+console.log(playRound(playerSelection, computerSelection));
+*/
