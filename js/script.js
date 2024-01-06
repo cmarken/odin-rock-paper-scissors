557f632e49bd296ff3998a9c8cfd4210f8af33be
Chris Marken <11192801+cmarken@users.noreply.github.com>
2024-01-06 09:58:09 +0100

    Set up files and pseudocode for algo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// randomly return 'Rock', 'Paper', 'Scissors'
+// getComputerChoice()
+
+// play a round and compare choices from player and computer, return a string
+// that declares a winner. Player selection needs to be case insenitive.
+// Replay round if tie.
+// playRound()
+
+// Used to contain playRound. Should make us play 5 games in a row. Keep score of
+// winner.
+// Display each round's winner.
+// Display the winner after each round.
+// game()
